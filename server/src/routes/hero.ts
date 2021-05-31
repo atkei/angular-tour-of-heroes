94aa2984e6f81058ddb3a6f3898ada3ca507b671
atktng <ktng.attc.19@gmail.com>
2021-05-31 23:21:14 +0900

    add searching to server

--- server/src/routes/hero.ts
+++ server/src/routes/hero.ts
@@ -4,7 +4,24 @@ import { Hero } from '../models/hero';
 const router = Express.Router();
 
 router.get('/heroes', async (req: Request, res: Response) => {
-  const hero = await Hero.find({});
+  const queryName = req.query.name;
+  console.log(`queryName=${queryName}`);
+
+  const conditions: any = {};
+  if (queryName) {
+    // https://docs.mongodb.com/manual/reference/operator/query/regex/
+    const param: any = {};
+    param['$regex'] = queryName;
+    param['$options'] = 'i';
+    conditions['name'] = param;
+  }
+
+  const heroes = await Hero.find(conditions);
+  return res.status(200).send(heroes);
+});
+
+router.get('/heroes/:id', async (req: Request, res: Response) => {
+  const hero = await Hero.findById(req.params.id);
   return res.status(200).send(hero);
 });
 
